7de810839ce6ce5357042c079397c59c4a8e379a
Atef Ben Ali <atef.bettaib@gmail.com>
2024-09-02 13:01:09 +0100

    fix(header): fix `display`

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 export function Header() {
   return (
-    <header className="px-10 flex justify-center items-center">
+    <header className="px-10 flex flex-col justify-center items-center">
       <img
         className="h-24 sm:h-32 md:h-48"
         src="./quran-competition.png"
         alt="banner"
       />
+      <h1>للقرآن الكريم</h1>
     </header>
   );
 }
